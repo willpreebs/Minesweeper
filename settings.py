aea394c6126cd5167193f08dfd87f316c1c1347b
willpreebs <wpriebe16124@gmail.com>
2022-10-03 15:43:21 -0400

    provided documentation for settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
+# Adjust these values to fit your screen
 WIDTH = 1140
 HEIGHT = 691
-
-X_AXIS = 30
-Y_AXIS = 16
-
 CELL_HEIGHT = 2
 CELL_WIDTH = 4
 
-MINES_COUNT = (X_AXIS * Y_AXIS) // 7
+# Change the following 3 values to adjust game settings
+X_AXIS = 30
+Y_AXIS = 16
+# One in ___ cells will be mines. Higher number is easier, lower is more difficult.
+MINES_CONCENTRATION = 7
+
+MINES_COUNT = (X_AXIS * Y_AXIS) // MINES_CONCENTRATION
